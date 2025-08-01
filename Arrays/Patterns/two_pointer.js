a53ed4b2a77450a39c28ec54b0ad4689d558bbea
Mani-420 <tahirabdulrehman46@gmail.com>
2025-08-01 15:43:20 +0500

    Two Pointer Technique

--- Arrays/Patterns/two_pointer.js
+++ Arrays/Patterns/two_pointer.js
@@ -1,27 +1,27 @@
 // --------------------------------- Opposite Directions ---------------------------------\\
 
-// function twoSumSorted(arr, target) {
-//   let left = 0;
-//   let right = arr.length - 1;
+function twoSumSorted(arr, target) {
+  let left = 0;
+  let right = arr.length - 1;
 
-//   while (left < right) {
-//     const sum = arr[left] + arr[right];
+  while (left < right) {
+    const sum = arr[left] + arr[right];
 
-//     if (sum === target) {
-//       return [left, right];
-//     } else if (sum < target) {
-//       left++;
-//     } else {
-//       right--;
-//     }
-//   }
-//   return [-1, -1];
-// }
+    if (sum === target) {
+      return [left, right];
+    } else if (sum < target) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+  return [-1, -1];
+}
 
-// const sortedArray = [1, 2, 3, 4, 6, 8, 9, 14, 15];
-// const target = 25;
-// result = twoSumSorted(sortedArray, target);
-// console.log('Two Sum Result:', result);
+const sortedArray = [1, 2, 3, 4, 6, 8, 9, 14, 15];
+const target = 25;
+result = twoSumSorted(sortedArray, target);
+console.log('Two Sum Result:', result);
 
 // --------------------------------- Same Directions ---------------------------------\\
 // ------------------ Slow Pointer and Fast Pointer ------------------\\
