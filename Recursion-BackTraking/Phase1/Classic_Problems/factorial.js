d1e60d7aa4aca5d12aa346e9c55a39400688ff25
Abdul Rehman Tahir <abdulrehman.tahir@hatzs.com>
2025-08-27 00:50:42 +0500

    Factorial with Recursion

---
+++ Recursion-BackTraking/Phase1/Classic_Problems/factorial.js
@@ -0,0 +1,9 @@
+function factorial(n) {
+  if (n === 0) {
+    return 1; // Base case
+  }
+  return n * factorial(n - 1); // Recursive call
+}
+
+console.log(factorial(5)); // 120
+console.log(factorial(0)); // 1
