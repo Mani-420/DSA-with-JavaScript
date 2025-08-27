e1204609aca0fa08d0c754a822d5be53621a46da
Abdul Rehman Tahir <abdulrehman.tahir@hatzs.com>
2025-08-27 22:16:46 +0500

    Basic Implementation

---
+++ LinkedList/Phase1/Topics/Basic_Creation.js
@@ -0,0 +1,32 @@
+class ListNode {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+
+  displayList() {
+    let current = this;
+    while (current) {
+      console.log(current.value);
+      current = current.next;
+    }
+  }
+}
+
+// Create nodes
+let start = new ListNode(1);
+let node2 = new ListNode(2);
+let node3 = new ListNode(3);
+let node4 = new ListNode(4);
+let node5 = new ListNode(5);
+
+// Link nodes
+start.next = node2;
+node2.next = node3;
+node3.next = node4;
+node4.next = node5;
+
+// Printing Node Values
+start.displayList();
+
+// start -> node2 -> node3 -> node4 -> node5 -> null
