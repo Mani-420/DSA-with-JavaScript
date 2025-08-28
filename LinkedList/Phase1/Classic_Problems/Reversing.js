777261a8d6e2d13e63470d88e789815f65fbd064
Abdul Rehman Tahir <tahirabdulrehman46@gmail.com>
2025-08-28 19:52:07 +0500

    Reversing a Linked List

--- LinkedList/Phase1/Classic_Problems/Reversing.js
+++ LinkedList/Phase1/Classic_Problems/Reversing.js
@@ -1,3 +1,10 @@
+class ListNode {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
 function reverseList(head) {
   let prev = null;
   let current = head;
@@ -10,6 +17,34 @@ function reverseList(head) {
   return prev; // New head
 }
 
+function traverse(head) {
+  let current = head;
+  let result = [];
+  while (current !== null) {
+    result.push(current.value);
+    current = current.next;
+  }
+  console.log(result.join(' -> '));
+}
+
+// Create nodes
+let head = new ListNode(1);
+let node2 = new ListNode(2);
+let node3 = new ListNode(3);
+let node4 = new ListNode(4);
+let node5 = new ListNode(5);
+
+// Link nodes
+head.next = node2;
+node2.next = node3;
+node3.next = node4;
+node4.next = node5;
+
 // Usage:
-let reversedHead = reverseList(newHead); // Reverses the list
-traverse(reversedHead); // 3 1
+console.log('Original List:');
+traverse(head); // 1 -> 2 -> 3 -> 4 -> 5
+
+let reversedHead = reverseList(head); // Reverses the list
+
+console.log('Reversed List:');
+traverse(reversedHead); // 5 -> 4 -> 3 -> 2 -> 1
